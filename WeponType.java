87f4b7ae02332296b144f04e39dfe4d69050e69a
SanjayCB <114073563+SanjayCB@users.noreply.github.com>
2023-01-05 10:02:18 -0800

    Add files via upload

---
+++ WeponType.java
@@ -0,0 +1,6 @@
+package com.xworkz.collectionsapp.jan5;
+
+public enum WeponType {
+	Shotgun,Rifle,Swords,BowAndArrow,Artillery,Spear,Pistol,BattleRifle,Dagger,MachineGun,Firearm,Handgun,Missile,SniperRifle,Cannon,Club,NuclearWeapons,Mace,Axe,AssaultRifle,Katana,Torpedo,Grenade,SubmachineGun,Lance;
+
+}
